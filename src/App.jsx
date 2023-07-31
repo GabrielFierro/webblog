07d4c4fdce608a88938ba58864eefe176d08e6fa
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-31 18:48:08 -0300

    fix: adding styles to the title and add some card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Card from './components/card/Card';
+
 function App() {
   return (
-    <div className='flex pt-12 justify-center bg-dark min-w-full h-screen'>
-      <h1 className='text-white font-bold text-4xl'>
-        Welcome to <span className='text-hAccent'>Webblog</span>
+    <div className='flex flex-col items-center py-12 justify-center bg-black min-w-full h-full px-2'>
+      <h1 className='font-bold text-4xl mb-12'>
+        <span className='text-white '>Welcome to </span>
+        <span className='animate-changing-color'>Webblog</span>
       </h1>
+      <div className='h-full w-full flex flex-row flex-wrap'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
