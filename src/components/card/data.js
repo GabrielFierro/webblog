e364201dc474926058dcb9a87c9bd05af9664341
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-31 20:09:42 -0300

    add: information about html, css, js and react in a file

---
+++ src/components/card/data.js
@@ -0,0 +1,51 @@
+import HtmlAvatar from '../../assets/images/gabrielfierro-html.webp';
+import CssAvatar from '../../assets/images/gabrielfierro-css.webp';
+import JsAvatar from '../../assets/images/gabrielfierro-js.webp';
+import ReactAvatar from '../../assets/images/gabrielfierro-react.webp';
+
+export const data = [
+  {
+    id: 0,
+    badge: 'HTML',
+    bgColor: 'bg-htmlAccentColor',
+    textColor: 'text-white',
+    title: '¿Qué es HTML?',
+    src: HtmlAvatar,
+    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
+    description:
+      'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
+  },
+  {
+    id: 1,
+    badge: 'CSS',
+    bgColor: 'bg-cssAccentColor',
+    textColor: 'text-white',
+    title: '¿Qué es CSS?',
+    src: CssAvatar,
+    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo azul',
+    description:
+      'Hojas de Estilo en Cascada (del inglés Cascading Style Sheets) o CSS es el lenguaje de estilos utilizado para describir la presentación de documentos HTML o XML. CSS es utilizado para diseñar y dar estilo a las páginas web, por ejemplo, alterando la fuente, color, tamaño y espaciado del contenido, dividirlo en múltiples columnas o agregar animaciones y otras características decorativas.'
+  },
+  {
+    id: 2,
+    badge: 'JavaScript',
+    bgColor: 'bg-jsAccentColor',
+    textColor: 'text-black',
+    title: '¿Qué es JavaScript?',
+    src: JsAvatar,
+    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo amarillo',
+    description:
+      'JavaScript (JS) es un lenguaje de programación ligero, interpretado, o compilado justo-a-tiempo (just-in-time) con funciones de primera clase. JavaScript es un lenguaje de programación basada en prototipos, multiparadigma, de un solo hilo, dinámico, con soporte para programación orientada a objetos, imperativa y declarativa (por ejemplo programación funcional).'
+  },
+  {
+    id: 3,
+    badge: 'React',
+    bgColor: 'bg-reactAccentColor',
+    textColor: 'text-black',
+    title: '¿Qué es React?',
+    src: ReactAvatar,
+    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo celeste',
+    description:
+      'La biblioteca para interfaces de usuario web y nativas. React te permite construir interfaces de usuario a partir de piezas individuales llamadas componentes.'
+  }
+];
