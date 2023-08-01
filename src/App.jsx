a31e23c733849932fdca2f4221e5160bd9f1b26b
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 18:08:07 -0300

    add: font size and scale on hover in each link

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
     <Header>
       <nav className='w-full mb-6'>
         <ul className='flex flex-row justify-evenly items-center w-auto'>
-          <li className='text-htmlAccentColor font-bold'>
+          <li className='text-htmlAccentColor font-bold text-xl hover:scale-125'>
             <Link to={`/html`}>HTML</Link>
           </li>
-          <li className='text-cssAccentColor font-bold'>
+          <li className='text-cssAccentColor font-bold text-xl hover:scale-125'>
             <Link to={`/css`}>CSS</Link>
           </li>
-          <li className='text-jsAccentColor font-bold'>
+          <li className='text-jsAccentColor font-bold text-xl hover:scale-125'>
             <Link to={`/js`}>JavaScript</Link>
           </li>
-          <li className='text-reactAccentColor font-bold'>
+          <li className='text-reactAccentColor font-bold text-xl hover:scale-125'>
             <Link to={`/react`}>React</Link>
           </li>
         </ul>
