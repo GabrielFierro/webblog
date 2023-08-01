d3a7ef0c0f21f9d3d780790961b29a2aea4ba256
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 17:22:26 -0300

    fix: deleting a span for the title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,6 @@ export default function Header({ children }) {
       className='flex flex-col items-center pt-9 justify-start bg-black min-w-full min-h-screen px-2'
     >
       <h1 className='font-bold text-4xl mb-4'>
-        <span className='text-white '>Welcome to </span>
         <span className='animate-changing-color'>Webblog</span>
       </h1>
       <p className='text-frenchGrey font-regular text-lg mt-2 mb-6'>
