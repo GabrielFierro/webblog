eb34005c8b476052e338c29efa6801fd737ae3ad
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 17:21:31 -0300

    fix: moving the outlet component to the bottom and adding a new paragraph with styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,18 @@ export default function Header({ children }) {
   return (
     <div
       id='pages'
-      className='flex flex-col items-center py-12 justify-start bg-black min-w-full min-h-screen px-2'
+      className='flex flex-col items-center pt-9 justify-start bg-black min-w-full min-h-screen px-2'
     >
-      <h1 className='font-bold text-4xl mb-12'>
+      <h1 className='font-bold text-4xl mb-4'>
         <span className='text-white '>Welcome to </span>
         <span className='animate-changing-color'>Webblog</span>
       </h1>
-      <Outlet />
+      <p className='text-frenchGrey font-regular text-lg mt-2 mb-6'>
+        Acá encontrarás información acerca de las tecnologías HTML, CSS,
+        JavaScript, y React.
+      </p>
       <div className='h-full w-full flex flex-row flex-wrap'>{children}</div>
+      <Outlet />
     </div>
   );
 }
