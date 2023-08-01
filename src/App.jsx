4a188ac96409b42448de5c196b759d09573fc520
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 20:58:11 -0300

    add: a variable which contains classes shared between the links

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import { Link } from 'react-router-dom';
 import Header from './components/header/Header';
 
 function App() {
+  const classes =
+    'px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer';
   return (
     <Header>
       <nav className='w-full mb-6'>
         <ul className='flex flex-row justify-evenly items-center w-auto'>
-          <li className='text-black bg-htmlAccentColor px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer'>
+          <li className={`text-black bg-htmlAccentColor ${classes}`}>
             <Link to={`/html`}>HTML</Link>
           </li>
-          <li className='text-white bg-cssAccentColor px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer'>
+          <li className={`text-white bg-cssAccentColor ${classes}`}>
             <Link to={`/css`}>CSS</Link>
           </li>
-          <li className='text-black bg-jsAccentColor px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer'>
+          <li className={`text-black bg-jsAccentColor ${classes}`}>
             <Link to={`/js`}>JavaScript</Link>
           </li>
-          <li className='text-black bg-reactAccentColor px-2 py-1 font-bold text-xl  hover:scale-125 rounded cursor-pointer '>
+          <li className={`text-black bg-reactAccentColor ${classes}`}>
             <Link to={`/react`}>React</Link>
           </li>
         </ul>
