627bdf030dc258f13536b7045f1284449cb3c7fc
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 02:17:10 -0300

    add: react router component to navigate between pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import Header from './components/header/Header.jsx';
+import HtmlCard from './components/visualhtml/Card.jsx';
+import CssCard from './components/visualcss/Card.jsx';
+import JsCard from './components/visualjs/Card.jsx';
+import ReactCard from './components/visualreact/Card.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/pages',
+    element: <Header />,
+    children: [
+      {
+        path: '/pages/html',
+        element: <HtmlCard />
+      },
+      {
+        path: 'css',
+        element: <CssCard />
+      },
+      {
+        path: 'js',
+        element: <JsCard />
+      },
+      {
+        path: 'react',
+        element: <ReactCard />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
