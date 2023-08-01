b8d2290ccad84f4b56f5a075b3c098127db6b21b
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 20:51:56 -0300

    fix: changing the colors, add padding and others properties to each  link

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
     <Header>
       <nav className='w-full mb-6'>
         <ul className='flex flex-row justify-evenly items-center w-auto'>
-          <li className='text-htmlAccentColor font-bold text-xl hover:scale-125'>
+          <li className='text-black bg-htmlAccentColor px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer'>
             <Link to={`/html`}>HTML</Link>
           </li>
-          <li className='text-cssAccentColor font-bold text-xl hover:scale-125'>
+          <li className='text-white bg-cssAccentColor px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer'>
             <Link to={`/css`}>CSS</Link>
           </li>
-          <li className='text-jsAccentColor font-bold text-xl hover:scale-125'>
+          <li className='text-black bg-jsAccentColor px-2 py-1 font-bold text-xl hover:scale-125 rounded cursor-pointer'>
             <Link to={`/js`}>JavaScript</Link>
           </li>
-          <li className='text-reactAccentColor font-bold text-xl hover:scale-125'>
+          <li className='bg-reactAccentColor px-2 py-1 font-bold text-xl hover:scale-125text-black hover:scale-125 rounded cursor-pointer '>
             <Link to={`/react`}>React</Link>
           </li>
         </ul>
