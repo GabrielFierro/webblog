a9eb27c9722cbf639dc06bb2fd41dc44449e3ed9
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-02 17:54:18 -0300

    add: the useNavigate to add functionality to the button

--- src/components/button/BackButton.jsx
+++ src/components/button/BackButton.jsx
@@ -1,4 +1,10 @@
-export default function BackButton({ goBack }) {
+import { useNavigate } from 'react-router-dom';
+
+export default function BackButton() {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <button
       onClick={goBack}
