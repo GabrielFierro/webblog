9c2c0c0ecd4f4e67eced8fbed3735060d33465a9
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-01 21:02:30 -0300

    fix: using the back button component instead of have the code in this component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import BackButton from '../button/BackButton';
 
 export default function Card({ data }) {
   const navigate = useNavigate();
@@ -45,28 +46,7 @@ export default function Card({ data }) {
   return (
     <div className='flex flex-row flex-wrap'>
       <div className='flex w-full pl-8'>
-        <button
-          onClick={goBack}
-          className='text-base rounded-r-none hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer hover:bg-greyLight bg-greyRegular text-white border duration-200 ease-in-out border-darkGrey transition'
-        >
-          <div className='flex leading-5'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='100%'
-              height='100%'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='feather feather-chevron-left w-5 h-5'
-            >
-              <polyline points='15 18 9 12 15 6'></polyline>
-            </svg>
-            Back
-          </div>
-        </button>
+        <BackButton onClick={goBack} />
       </div>
       {card}
     </div>
