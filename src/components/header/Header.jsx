7a3675b5cd56befe36b3e9ba3f43243216819b1b
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-02 17:57:37 -0300

    add: the font merrieweather to the title and the description

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,10 +6,10 @@ export default function Header({ children }) {
       id='pages'
       className='flex flex-col items-center pt-9 justify-start bg-black min-w-full min-h-screen px-2'
     >
-      <h1 className='font-bold text-4xl mb-4'>
+      <h1 className='font-merriweather-regular font-bold text-4xl mb-4'>
         <span className='animate-changing-color'>Webblog</span>
       </h1>
-      <p className='text-frenchGrey font-regular text-lg mt-2 mb-6'>
+      <p className='text-frenchGrey font-merriweather-regular text-lg mt-2 mb-6'>
         Acá encontrarás información acerca de las tecnologías HTML, CSS,
         JavaScript, y React.
       </p>
