5bdb5439f78002dda5ef95903c8f79d2e14bc738
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-02 18:33:19 -0300

    fix: changing the way to get data from the array

--- src/components/visualhtml/htmlData.js
+++ src/components/visualhtml/htmlData.js
@@ -1,48 +1,50 @@
 import HtmlAvatar from '../../assets/images/gabrielfierro-html.webp';
 
+// Función para crear objetos con las propiedades comunes
+function createData(id, badge, title, src, alt, description) {
+  return {
+    id: id,
+    badge: badge,
+    bgColor: `bg-htmlAccentColor`,
+    textColor: 'text-black',
+    title: title,
+    src: src,
+    alt: alt,
+    description: description
+  };
+}
+
 export const data = [
-  {
-    id: 0,
-    badge: 'HTML',
-    bgColor: 'bg-htmlAccentColor',
-    textColor: 'text-black',
-    title: '¿Qué es HTML?',
-    src: HtmlAvatar,
-    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
-    description:
-      'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
-  },
-  {
-    id: 1,
-    badge: 'HTML',
-    bgColor: 'bg-htmlAccentColor',
-    textColor: 'text-black',
-    title: '¿Qué es HTML?',
-    src: HtmlAvatar,
-    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
-    description:
-      'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
-  },
-  {
-    id: 2,
-    badge: 'HTML',
-    bgColor: 'bg-htmlAccentColor',
-    textColor: 'text-black',
-    title: '¿Qué es HTML?',
-    src: HtmlAvatar,
-    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
-    description:
-      'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
-  },
-  {
-    id: 3,
-    badge: 'HTML',
-    bgColor: 'bg-htmlAccentColor',
-    textColor: 'text-black',
-    title: '¿Qué es HTML?',
-    src: HtmlAvatar,
-    alt: 'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
-    description:
-      'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
-  }
+  createData(
+    0,
+    'HTML',
+    '¿Qué es HTML?',
+    HtmlAvatar,
+    'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
+    'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
+  ),
+  createData(
+    1,
+    'HTML',
+    '¿Qué es HTML?',
+    HtmlAvatar,
+    'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
+    'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
+  ),
+  createData(
+    2,
+    'HTML',
+    '¿Qué es HTML?',
+    HtmlAvatar,
+    'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
+    'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
+  ),
+  createData(
+    3,
+    'HTML',
+    '¿Qué es HTML?',
+    HtmlAvatar,
+    'Avatar acerca del autor Gabriel Fierro con un fondo naranja',
+    'HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) es el componente más básico de la Web. Define el significado y la estructura del contenido web.'
+  )
 ];
